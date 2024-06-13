54e59b2de2aa677dd1fc8193151a01528080a2c4
Xail-Carballo <xailcarballo@gmail.com>
2024-06-12 20:23:34 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 var swiper = new Swiper('.contenedor-slider', {
     effect: 'coverflow',
-    grabCursor: false,
+    grabCursor: true,
     centeredSlides: true,
     slidesPerView: '1',
     coverflowEffect: {
         rotate: 15,
-        stretch: 700,
-        depth: 200,
+        stretch: 600,
+        depth: 400,
         modifier: 0.5,
         slideShadows: false,
     },
-    loop: false,
+    loop: true,
 
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+
+navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+},
 });
-    // ¡para los botones
-    document.querySelector('.swiper-button-prev').addEventListener('click', function () {
-        swiper.slidePrev();
-    });
-    
-    document.querySelector('.swiper-button-next').addEventListener('click', function () {
-        swiper.slideNext();
-    });
+// ¡para los botones
+document.querySelector('.swiper-button-prev').addEventListener('click', function () {
+    swiper.slidePrev();
+});
 
+document.querySelector('.swiper-button-next').addEventListener('click', function () {
+    swiper.slideNext();
+});
